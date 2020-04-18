c6bb33d6ac61b6feb73fb06837d92ff1414859dc
František Gič <frantisek.gic@gmail.com>
2020-04-18 12:51:29 +0200

    Refactor main script for generation extraction and selecting parents

--- src/script.ts
+++ src/script.ts
@@ -1,41 +1,30 @@
 import Garden from './garden';
 import input from './input/test1';
 
-const garden = new Garden(input);
+const startPopulation = 200;
+const generations = {
+    actual: 1,
+    max: 100,
+};
+const population: Garden[] = [];
 
-garden.walk();
-garden.print();
-console.log("Garden.score: ",garden.score);
+// Generate first starting population
+for (let i = 0; i < startPopulation; i++) {
+    population.push(new Garden(input));
+}
 
-/*
-function Genetický-Algoritmus(populácia, Vyhodnocovacia-Funkcia)
-returns riešenie inputs: populácia, množina riešení
-F-
--
-	TT
-Vyhodnocovacia-Funkcia, funkcia, ktorá vyjadruje úspešnosť daného riešenia
-until nejaké riešenie je dostatočne úspešné
-return najlepšie riešenie v populácii podľa Vyhodnocovacej-
-Funkcie
-end
- */
+while (generations.actual < generations.max) {
+    // Sort population by score
+    population.sort((a, b) => {
+        return a.score - b.score;
+    });
 
-/*
-0  ŠTART   : vytvor náhodnú populáciu n chromozómov
-1  FITNES : vyhodnoť fitnes f(x) každého chromozómu v
-populácii
-2 REPRODUKCIA ⇒0 VÝBER
-	: založený na f(x)
-: kríženie chromozómov
-	: zmutuj chromozómy
-	: odstráň alebo prijmi nový
-chromozóm
-1 REKOMBINÁCIA
-2 MUTÁCIA
-3 PRIJATIE
-3  NÁHRADA : nahraď starú populáciu novou
-4  TEST    : otestuj, či je v populácii riešenie
-problému
-5 CYKLUS : opakuj kroky 1 – 4 dovtedy, až nejaký chromozóm prejde úspešne testom
+    // If the whole garden is raked in this population
+    if (population[population.length - 1].score == 1) break;
 
- */
+    // vyber jedincov
+
+    // mutacia
+
+    generations.actual++;
+}
