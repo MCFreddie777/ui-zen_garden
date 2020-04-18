9731ee0dec448f99f84dbe4211e24c6511b33247
František Gič <frantisek.gic@gmail.com>
2020-04-18 17:19:27 +0200

    Refactor monk to be realistic gene

--- src/monk.ts
+++ src/monk.ts
@@ -1,24 +1,69 @@
 export class Monk {
-    public position!: Coordinates;
-    public direction: 'H' | 'V';
+    constructor(public position: Coordinates, public direction: 'H' | 'V') {}
 
-    constructor(size: Coordinates) {
-        this.position = { x: 0, y: 0 };
+    /**
+     * Recursive function which goes through the garden, trying to find the exit.
+     *
+     * @param monk The instance of monk having position
+     * @param garden Instance of garden
+     * @param mark The mark that should leave monk on the raked field
+     */
+    walk(monk: Monk, garden: string[][], mark: string): Statuses {
+        // Whether the monk could exit the garden
+        if (
+            !monk.inBounds(monk.position, {
+                x: garden.length,
+                y: garden[0].length,
+            })
+        )
+            return Statuses.SUCCESS;
 
-        // Random from first row
-        if (Math.round(Math.random())) {
-            this.position.x = 0;
-            this.position.y = random(0, size.y - 1);
-            this.direction = 'V';
+        // If hit rock or another pathway
+        if (garden[monk.position.x][monk.position.y] !== '0')
+            return Statuses.TURN;
+
+        for (let i = 0; i < 2; i++) {
+            // Nest into next step
+            const result: Statuses = monk.walk(
+                new Monk(monk.getNextStep(), monk.direction),
+                garden,
+                mark
+            );
+
+            // Mark the current pathway
+            if (result === Statuses.SUCCESS) {
+                garden[monk.position.x][monk.position.y] = mark;
+                return Statuses.SUCCESS;
+            }
+            // Toggle direction
+            else if (result === Statuses.TURN) {
+                monk.direction = monk.direction == 'H' ? 'V' : 'H';
+            }
+            // Cannot mark the pathway, this gene failed
+            else return Statuses.FAIL;
         }
-        // Random from last column
-        else {
-            this.position.x = random(0, size.x - 1);
-            this.position.y = size.y - 1;
-            this.direction = 'H';
-        }
+        // Unreachable step
+        return Statuses.FAIL;
     }
 
+    /**
+     * Check whether coordinates are inside the garden
+     *
+     * @param position Coordinates, position of the monk
+     * @param size Size of the garden
+     */
+    inBounds(position: Coordinates, size: Coordinates): boolean {
+        return (
+            position.x < size.x &&
+            position.x >= 0 &&
+            position.y < size.y &&
+            position.y >= 0
+        );
+    }
+
+    /**
+     * Returns a position in Gaussian 2D coordinates of a next step according to currently set direction
+     */
     getNextStep(): Coordinates {
         if (this.direction == 'H')
             return {
@@ -38,6 +83,8 @@ export interface Coordinates {
     y: number;
 }
 
-function random(min: number = 0, max: number): number {
-    return Math.floor(Math.random() * (max - min) + min);
+export enum Statuses {
+    FAIL,
+    SUCCESS,
+    TURN,
 }
